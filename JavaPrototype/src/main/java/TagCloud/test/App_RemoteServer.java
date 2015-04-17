d0f7917b51389b95d91a782861deef2997c4f129
tb21 <tb21@users.noreply.github.com>
2015-04-17 11:13:59 +0200

    Simple test class which connects to an existing and running elasticsearch instance.

---
+++ JavaPrototype/src/main/java/TagCloud/test/App_RemoteServer.java
@@ -0,0 +1,43 @@
+package TagCloud.test;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.Node;
+
+import static org.elasticsearch.node.NodeBuilder.*;
+import static org.elasticsearch.common.xcontent.XContentFactory.*;
+/**
+ * Hello world!
+ *
+ */
+public class App_RemoteServer 
+{
+	public static void main( String[] args ) throws ElasticsearchException, IOException
+    {
+
+		Settings settings = ImmutableSettings.settingsBuilder()
+		        .put("cluster.name", "elasticsearch").build();
+		Client client =    new TransportClient(settings)
+							.addTransportAddress(new InetSocketTransportAddress("localhost", 9300))
+							.addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+		//Add transport addresses and do something with the client...
+		
+		IndexResponse response2 = client
+				.prepareIndex("games", "game", "1")
+				.setSource(
+						jsonBuilder().startObject().field("title", "battlefield hardline")
+								.field("postDate", new Date())
+								.field("message", "GTA, motherfucker!")
+								.endObject()).execute().actionGet();
+    }   
+}
