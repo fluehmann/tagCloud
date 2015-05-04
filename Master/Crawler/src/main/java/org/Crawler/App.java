e1452e1bc7bcc17cdc90039519e2ac6cd1821769
Simon Flühmann <simon.fluehmann@students.fhnw.ch>
2015-05-04 10:08:17 +0200

    Reset

---
+++ Master/Crawler/src/main/java/org/Crawler/App.java
@@ -0,0 +1,13 @@
+package org.Crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
