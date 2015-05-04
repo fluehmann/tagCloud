3e7e53171f800d2329687a399b3fca89f1adf075
Simon Flühmann <simon.fluehmann@students.fhnw.ch>
2015-05-04 10:45:25 +0200

    Crawling part added

---
+++ Project/Crawling/src/main/java/org/Crawling/CrawlCallable.java
@@ -0,0 +1,28 @@
+package org.Crawling;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.Callable;
+
+import org.jsoup.nodes.Document;
+
+public class CrawlCallable implements Callable {
+	
+	private final String startURL;
+	
+	public CrawlCallable(String startURL) {
+		this.startURL = startURL;
+	}
+
+	public HashMap<String, Document> call() throws Exception {
+		
+		URL url = new URL(startURL);
+		
+		
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
---
+++ Project/Crawling/src/main/java/org/Crawling/WebCrawler.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.Crawling;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * Implementation of a Webcrawler
+ * @author simonfluhmann
+ *
+ */
+public class WebCrawler implements Crawler{
+
+	private final int POOL_SIZE = 50;
+	
+	public WebCrawler(String url){
+		
+		// put this in webinterface!! not THReadsafe
+		if(!url.startsWith("http")) {
+			url =  "http://" + url;
+			crawl(url);
+		}
+		
+	}
+	
+//	return a hashmap => url als ID und page als 'Site' Objekt
+	
+	
+	/**
+     * Crawls the www starting at {@code src} 
+     * @param src the URI to start crawling any resource
+     * @return a list of uris that are reachable from {@code src}
+     */
+	public List<String> crawl(final String startUrl) {
+		
+		/* Contains all discovered urls. */
+		final Set<String> urlsFound = new HashSet<String>();
+		
+		/* Contains the urls to be visited. */
+		final Queue<String> urlsToVisit = new LinkedList<String>();
+				
+		urlsToVisit.add(startUrl);
+		
+//		Document doc;
+//		try {
+//			doc = Jsoup.connect("http://example.com/").get();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String title = doc.title();
+//		
+//		System.out.println(doc);
+
+		
+		
+		return new ArrayList<String>();
+		
+	}
+}
